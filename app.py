0d57ef46ba7180061a70251b2bcdb180b9b28ea6
jamistyping <6640265622@student.chula.ac.th>
2023-12-06 01:19:53 +0700

    de guru

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import openai
+import pandas as pd
+import json
+import time
+
+# Get API user keys from sidebar called OpenAI API Keys
+user_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+client = openai.OpenAI(api_key=user_api_key)
+prompt = """ Act as an expert German-English translator. You will be given a German passage 
+        and then select some interesting german vocabulary in A2 level or upper that appear in the text.
+        List the vocabulary in a JSON array, one vocabulary per line.
+        Each line should contains 3 categories:
+            1) 'Words' - German vocabulary in A2 level or upper appeared in the text received.
+                    If it is a noun, its definite article('der', 'die', 'das') of the noun must be included.
+                    If it is a verb an adjective, state it in its infinitive form. 
+                    Definite article must not be selected as vocabulary on the list.
+            2) 'Part of Speech' - noun, verb, adjective, adverb, or conjunction. write in lower case.
+            3) 'Meaning' - meaning in english
+            (Sort the vocabulary in order of words' difficulty.)
+        """
+prompt_sum = """ Act as an expert German-English translator. 
+            Summerize the german text in simple 2-3 English sentences.
+            """
+
+
+st.title('🇩🇪 _German Text Guru_')
+st.markdown('To improve your German vocabulary, we will provide you the meaning of each word and its part of speech from your text.')
+st.divider()
+
+# Get user input in a text box
+user_input = st.text_area("Enter German text.", height=200, value='German text here...')
+if not user_input:
+    st.warning('You have not enter any German text...🥺')
+
+# check box if the user wants the summerized text in english
+summerize = st.checkbox('Summerize the text in English', value=False)
+if summerize:
+    st.write(':grey[*We will do our best to help you better understand the text!*]')
+
+
+
+#submit button 
+if st.button('Submit', type='primary'):
+    messages_so_far = [
+        {'role': 'system', 'content': prompt},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages_so_far
+    )
+    if summerize:
+        messages_sum = [
+            {'role': 'system', 'content': prompt_sum},
+            {'role': 'user', 'content': user_input},
+        ]
+        response_sum = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages_sum
+        )
+        st.markdown('Summerized text in English:')
+        container = st.container(border=True)
+        container.write(response_sum.choices[0].message.content)
+
+    # create a table with 3 columns: words, part of speech, meaning
+    st.markdown('German Vocabulary:')
+    vocab_all = response.choices[0].message.content
+    vocab = json.loads(vocab_all)
+    vocab_table = pd.DataFrame.from_dict(vocab)
+    st.table(vocab_table)
+
+   
+
+
+
+
+
+    
